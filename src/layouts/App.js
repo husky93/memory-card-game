7c72a4f94a8acfb4db49ad715989244996e491b3
Maciej Sroka <sroka.maciej93@gmail.com>
2022-08-31 18:07:02 +0200

    Display spinner until all images load

--- src/layouts/App.js
+++ src/layouts/App.js
@@ -1,25 +1,57 @@
-import logo from '../assets/logo.svg';
+import React, { useState, useRef, useEffect } from 'react';
+import Spinner from '../components/Spinner';
 import '../assets/styles/App.css';
+import importAllImages from '../modules/importAllImages';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+  const [loaded, setLoaded] = useState(false);
+  const [images, setImages] = useState([]);
+  const isFirstRender = useRef(true);
+
+  useEffect(() => {
+    if (isFirstRender.current) {
+      Promise.all(loadImages())
+        .then((values) => {
+          // values.forEach((module) => {
+          //   const img = <img src={module.default}></img>;
+          // });
+          console.log(values);
+          setLoaded(true);
+        })
+        .catch((err) => console.log(err));
+    }
+    isFirstRender.current = false;
+  }, []);
+
+  const loadImages = () => {
+    const importedImages = importAllImages();
+    return importedImages.map((image) => {
+      return new Promise((resolve) => {
+        const img = new Image();
+        img.onload = () => {
+          setImages((oldArray) => [...oldArray, img]);
+          resolve(image);
+        };
+        img.src = image;
+      });
+    });
+  };
+
+  if (!loaded) {
+    return (
+      <div className="App">
+        <Spinner />
+      </div>
+    );
+  } else {
+    return (
+      <div className="App">
+        {images.map((image) => (
+          <img src={image.src} />
+        ))}
+      </div>
+    );
+  }
+};
 
 export default App;
---
+++ src/modules/importAllImages.js
@@ -0,0 +1,33 @@
+import AryaImg from '../assets/images/aryastark.webp';
+import BrienneImg from '../assets/images/brienneoftarth.webp';
+import CerseiImg from '../assets/images/cecrseilannister.webp';
+import DaenerysImg from '../assets/images/daenerys.webp';
+import JamieImg from '../assets/images/jamielannister.webp';
+import JonImg from '../assets/images/jonsnow.webp';
+import MaergeryImg from '../assets/images/margerytyrell.webp';
+import RobertImg from '../assets/images/robertbaratheon.webp';
+import SandorImg from '../assets/images/sandorclegane.webp';
+import TheonImg from '../assets/images/theongreyjoy.webp';
+import TyrionImg from '../assets/images/tyrionlannister.webp';
+import WalderImg from '../assets/images/walderfrey.webp';
+import LoadTest from '../assets/images/load-testing-image.png';
+
+const importAllImages = () => {
+  return [
+    AryaImg,
+    BrienneImg,
+    CerseiImg,
+    DaenerysImg,
+    JamieImg,
+    JonImg,
+    MaergeryImg,
+    RobertImg,
+    SandorImg,
+    TheonImg,
+    TyrionImg,
+    WalderImg,
+    LoadTest,
+  ];
+};
+
+export default importAllImages;
