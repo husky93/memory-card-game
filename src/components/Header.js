45514cf939a4f8621cb0d129b0b85830d5b35f0d
Maciej Sroka <sroka.maciej93@gmail.com>
2022-09-01 11:36:15 +0200

    Add basic Header component template

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header className="header">
+      <h1 className="logo">Game of Thrones</h1>
+      <strong className="logo-subtitle">Memory card game</strong>
+    </header>
+  );
+};
+
+export default Header;
